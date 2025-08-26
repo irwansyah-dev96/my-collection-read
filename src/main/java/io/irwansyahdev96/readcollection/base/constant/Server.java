e4336279f8bae85e1d960393838950e72ec6dc8f
irwansyah10010 <irwanbae33@gmail.com>
2025-08-26 09:57:49 +0700

    base is progress

--- src/main/java/io/irwansyahdev96/readcollection/base/constant/Server.java
+++ src/main/java/io/irwansyahdev96/readcollection/base/constant/Server.java
@@ -1,7 +1,6 @@
 package io.irwansyahdev96.readcollection.base.constant;
 
 public class Server {
-    public static String SERVER_CORE = "http://localhost:8081/mbc/";
     public static String PATH_STATUS = "status/produce";
     public static String PATH_BOOK = "books/produce";
     public static String PATH_CATEGORY = "categories/produce";
